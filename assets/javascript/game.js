ff95fadf1467f16ad5fbd3b33039f80c54acb047
unknown <jjernigan1065@gmail.com>
2018-03-03 09:38:25 -0500

    got letters to replace underscores

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,47 +14,60 @@ var game = {
 
             // making sure the game doesn't pick a new word every time
         if (!this.computerGuess) {
+            console.log("inside setting answer");
             // picking a random word from the word bank
             this.computerGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
             console.log(this.computerGuess);
             // do something the same amount of times as the length of the word to guess
-            for (i = 0; i < this.computerGuess.length; i++) {
+            for (var i = 0; i < this.computerGuess.length; i++) {
                 // var space = this.computerGuess.indexOf(" ");
 
                 game.answer.push(this.computerGuess[i]);
 
                 console.log(game.answer);
                 game.underScore.push("___ ");
-        }
-
-        if (game.userCheck.includes(userGuess)){
-
-            for (i = 0; i < game.answer.length; i++){
-                if (game.answer[i] === userGuess){
-                    game.underScore[i] = userGuess;
-                    // game.underScore.push(userGuess);
-                }
             }
         }
 
+        if (game.userCheck.includes(userGuess)){
+            // debugger;
+            console.log("i'm inside of user check boolean");
+            for ( var j = 0; j < game.answer.length; j++){
+                if (game.answer[j] === userGuess){
+                    console.log("inside for loop checking userguess against answer arr");
+                    game.underScore[j] = userGuess;
+                    console.log(game.underScore[j]);
+                    // game.underScore.push(userGuess);
+
+                    }
+
+
+                }
+            if(game.answer.indexOf("___ ") === -1){
+                game.wins++;
+            }
+            }
+
+
+        console.log(game.answer);
+        console.log(game.underScore);
+
+        console.log("this is answer");
 
 
 
-
-            console.log("this is answer");
-
-
-        }
         // if (game.answer.includes(userGuess)) {
 
         // }
 
 
         document.querySelector(".answer").innerHTML = game.underScore.join(" ");
+        document.querySelector(".wins").textContent = game.wins;
         console.log(game.underScore.join(" "));
         console.log(game.answer);
 
-    }};
+    }
+};
     // };
 
 document.onkeyup = function(){
